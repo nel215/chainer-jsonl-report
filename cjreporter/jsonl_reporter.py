87053c8535401ebd874644817e287d5b61290dad
nel215 <otomo.yuhei@gmail.com>
2018-12-08 19:01:59 +0900

    Separate LatestLog

--- cjreporter/jsonl_reporter.py
+++ cjreporter/jsonl_reporter.py
@@ -1,24 +1,7 @@
 import os
 import json
 from chainer.training import extensions
-
-
-class LatestLog:
-
-    def __init__(self):
-        self._record = {}
-        self._size = 0
-
-    def __len__(self):
-        return self._size
-
-    def __getitem__(self, idx):
-        assert idx == self._size-1
-        return self._record
-
-    def update(self, record):
-        self._record = record
-        self._size += 1
+from .latest_log import LatestLog
 
 
 class JsonlReport(extensions.LogReport):
